d40e41a86ea90e414153aa6b2ac76aa781fa3bab
Geoff Low <glow@mdsol.com>
2022-02-07 11:15:50 +0000

    Testing labels and type enumerations

--- typescript/dataset.ts
+++ typescript/dataset.ts
@@ -1,9 +1,43 @@
+/**
+ * Enumerated set of Permissible Item Types
+ */
 export type ItemType = 'string' | 'integer' | 'float' | 'double';
 
+/**
+ * Definition for Item in the Dataset
+ */
 export interface ItemDescription {
+    /**
+     * Unique Identifier for Item.
+     *
+     * @OID.type string
+     * @OID.minimum 1
+     * @OID.maximum 1
+     */
     OID: string;
+    /**
+     * Name for Item.
+     *
+     * @name.type string
+     * @name.minimum 1
+     * @name.maximum 1
+     */
     name: string;
+    /**
+     * Label for Item
+     * 
+     * @label.type string
+     * @label.minimum 0
+     * @label.maximum 1
+     */
     label?: string;
+    /**
+     * Item data type
+     * 
+     * @type.type ItemType
+     * @type.minimum 0
+     * @type.maximum 
+     */
     type?: ItemType;
 }
 
